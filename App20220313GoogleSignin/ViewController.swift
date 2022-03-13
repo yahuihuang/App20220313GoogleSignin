5f3e2d9e421c37af1c6bb28be8bc8d89ab17b900
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-13 11:22:38 +0800

    手動登入google

--- App20220313GoogleSignin/ViewController.swift
+++ App20220313GoogleSignin/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signinGoogleAction(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
 
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+          if let error = error {
+            // ...
+            return
+          }
+
+          guard
+            let authentication = user?.authentication,
+            let idToken = authentication.idToken
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: authentication.accessToken)
+
+          // ...
+        }
+    }
+    
 }
 
